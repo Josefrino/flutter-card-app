42292c8ac730e51d511e1ef05243ff01ce861af0
Josefrino Fernando Fonseca Vieira <josefrino.vieira@gmail.com>
2023-05-01 23:12:43 +0100

    custom circle avatar

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,65 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(
-      const MaterialApp(
-        home: MyApp(),
-      ),
-    );
+// void main() => runApp(
+//       const MaterialApp(
+//         home: MyApp(),
+//       ),
+//     );
+
+// class MyApp extends StatelessWidget {
+//   const MyApp({Key? key}) : super(key: key);
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return const Scaffold(
+//       body: Center(
+//         child: Text('MiCard'),
+//       ),
+//     );
+//   }
+// }
+
+void main() {
+  runApp(
+    const MaterialApp(
+      home: MyApp(),
+    ),
+  );
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
+      backgroundColor: Colors.blueGrey.shade900,
       body: Center(
-        child: Text('MiCard'),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              width: 150,
+              height: 150,
+              decoration: BoxDecoration(
+                image: const DecorationImage(
+                  image: AssetImage('images/me.png'),
+                  fit: BoxFit.fill,
+                ),
+                borderRadius: BorderRadius.circular(200),
+              ),
+            ),
+            const Text('data'),
+            const Text('data'),
+            const Padding(
+              padding: EdgeInsets.symmetric(horizontal: 24, vertical: 10),
+              child: Divider(
+                height: 1,
+                color: Colors.white,
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
