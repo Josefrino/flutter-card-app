b169a657801c2a01bc528f007173e2d146937c8a
Josefrino Fernando Fonseca Vieira <josefrino.vieira@gmail.com>
2023-05-02 21:28:50 +0100

    boat image added

--- lib/main.dart
+++ lib/main.dart
@@ -37,21 +37,14 @@ class MyApp extends StatelessWidget {
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Container(
-              width: 150,
-              height: 150,
-              decoration: BoxDecoration(
-                image: const DecorationImage(
-                  image: AssetImage('images/me.png'),
-                  fit: BoxFit.fill,
-                ),
-                borderRadius: BorderRadius.circular(200),
-              ),
+          children: const [
+            CircleAvatar(
+              radius: 60,
+              backgroundImage: AssetImage('images/boat.png'),
             ),
-            const Text('data'),
-            const Text('data'),
-            const Padding(
+            Text('data'),
+            Text('data'),
+            Padding(
               padding: EdgeInsets.symmetric(horizontal: 24, vertical: 10),
               child: Divider(
                 height: 1,
